060dc7bcc117f75505357f47e8d8e5b531d9fde2
iashu2k <iashu2k@gmail.com>
2020-05-11 17:30:43 +0530

    home page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 void main() {
   runApp(CodingPal());
@@ -14,13 +15,96 @@ class CodingPal extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('CodingPal',
-          style: TextStyle(
-            fontFamily: 'PressStart2P',
-          ),
+          actions: <Widget>[
+            CircleAvatar(
+              radius: 18,
+              child: ClipOval(
+                child: Image.network(
+                  'https://www.tuktukdesign.com/wp-content/uploads/2020/01/profile-icon-vector.jpg',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 15.0,
+            ),
+          ],
+          title: Text(
+            'CodingPal',
+            style: TextStyle(
+              fontFamily: 'PressStart2P',
+            ),
           ),
         ),
-        body: Text('Hello World'),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                                      child: LogoCards(
+                      logoname: 'codechef.jpg',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                                      child: LogoCards(
+                      logoname: 'codeforces.png',
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  LogoCards(
+                    logoname: 'hackerearth.png',
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  LogoCards(
+                    logoname: 'hackerrank.png',
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  LogoCards(
+                    logoname: 'leetcode.jpg',
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  LogoCards(
+                    logoname: 'topcoder.jpg',
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LogoCards extends StatelessWidget {
+  LogoCards({this.logoname});
+  final String logoname;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset('images/$logoname'),
+      //margin: EdgeInsets.all(15.0),
+      padding: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: kcardColor,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
