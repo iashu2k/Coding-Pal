713cd0d55ffab1f3d39ceac22efa09fed6b45abf
iashu2k <iashu2k@gmail.com>
2020-05-22 19:33:33 +0530

    apis secured, google analytics added

--- lib/services/contestsinfo.dart
+++ lib/services/contestsinfo.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
 import 'current_date_time.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_config/flutter_config.dart';
 
 class ContestInfo {
   
@@ -10,12 +13,17 @@ class ContestInfo {
   CurrentTime currentTime = CurrentTime();
   String currenttime = CurrentTime().getcurrent();
   String nextmonth = CurrentTime().getnextMonth();
-  String userid = 'iashu2k';
-  String apiKey = '81c3bece1c696d9f9534fefc4b2627d54f396e0a';
+
 
   Future contestslist() async {
+    
 
     try {
+      WidgetsFlutterBinding.ensureInitialized();
+    await FlutterConfig.loadEnvVariables();
+
+    String userid = FlutterConfig.get('USER_ID').toString();
+    String apiKey = FlutterConfig.get('API_KEY').toString();
       String url = '''
 https://clist.by/api/v1/json/contest/?username=$userid&api_key=$apiKey&end__gt=$currenttime&end__lt=$nextmonth&order_by=start''';
     http.Response response = await http.get(url);
